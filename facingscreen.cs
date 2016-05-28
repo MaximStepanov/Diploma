2633543c3a43add2dc17040a2acbac4ce3af88ee
Alone4ik <kiper94@mail.ru>
2016-05-28 20:22:38 +0300

    Add files via upload

---
+++ facingscreen.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class facingscreen : MonoBehaviour {
+	public Transform target;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target != null) {
+			transform.LookAt(transform.position + target.transform.rotation * Vector3.forward,
+				target.transform.rotation * Vector3.up);
+			transform.Rotate (new Vector3 (-90, 0, 0));
+		}
+	}
+}
